c907ff1c01bd278d8bf1950670bc51fcac79968f
om-lakade-coditas <om.lakade@coditas.com>
2026-06-24 11:09:05 +0530

    implemented initial setup

---
+++ fieldops-app/src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import styles from "./Header.module.scss";
+
+
+const Header = () => {
+  return (
+    <>
+        <div className={styles.Header}>
+            <div className={styles.CompanyName}>FieldOps</div>
+        </div>
+    </>
+)
+}
+
+export default Header
